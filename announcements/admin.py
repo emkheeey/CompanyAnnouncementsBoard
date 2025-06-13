8b919249efe9d9a491f033b52e5a102608d8ce75
emkheeey <marielkristineobsid.lorena@bicol-u.edu.ph>
2025-06-13 15:42:31 +0800

    Update admin.py

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import Announcement
 
-@admin.register(Announcement)
-class AnnouncementAdmin(admin.ModelAdmin):
-    list_display = ['title', 'posted_by', 'date']
-    list_filter = ['date', 'posted_by']
-    search_fields = ['title', 'message']
-    readonly_fields = ['id', 'date']
-    ordering = ['-date']
+admin.site.register(Announcement)
